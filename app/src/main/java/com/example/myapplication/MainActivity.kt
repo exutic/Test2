475a913bbe9cfc7f41bce59b8339723a94054561
middlehmp <133151035+middlehmp@users.noreply.github.com>
2023-06-27 17:41:36 +0330

    FIrst Ui Update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,69 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvWelcome: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvWelcome = findViewById(R.id.tvWelcome)
+    }
 
-        val condition:Boolean = false
+    @SuppressLint("SetTextI18n")
+    fun openLoginDialog(view: View) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater: LayoutInflater = this.layoutInflater
+        val dialogView: View = inflater.inflate(R.layout.dialog_login, null)
+        dialogBuilder.setView(dialogView)
 
-        print(condition)
+        val etName = dialogView.findViewById<EditText>(R.id.etName)
+
+        dialogBuilder.setTitle("Login")
+        dialogBuilder.setMessage("Enter your name:")
+        dialogBuilder.setPositiveButton("Login") { dialogInterface: DialogInterface, _: Int ->
+            val name = etName.text.toString()
+            tvWelcome.text = "Welcome, $name!"
+            dialogInterface.dismiss()
+        }
+        dialogBuilder.setNegativeButton("Cancel") { dialogInterface: DialogInterface, _: Int ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = dialogBuilder.create()
+        alertDialog.show()
+    }
+
+    fun startNewGame(view: View) {
+        // Handle "New Game" button click
+        val intent = Intent(this, NewGameActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun continueGame(view: View) {
+        // Handle "Continue" button click
+        val intent = Intent(this, NewGameActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun openAboutScreen(view: View) {
+        // Handle "About" button click
+        val intent = Intent(this, AboutActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun exitGame(view: View) {
+        // Handle "Exit Game" button click
+        finishAffinity() // Close the application
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000 // Delay in milliseconds (3seconds)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+    }
+}
