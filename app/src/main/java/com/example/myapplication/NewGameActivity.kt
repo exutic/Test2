3666b7178a432f0f8c949a7b74f64cff4a7fdbb6
Aryan Goodarzi Nik <aryan.goodarzi@yahoo.com>
2023-07-02 20:47:11 +0330

    Update NewGameActivity.kt

--- app/src/main/java/com/example/myapplication/NewGameActivity.kt
+++ app/src/main/java/com/example/myapplication/NewGameActivity.kt
@@ -8,4 +8,11 @@ class NewGameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game)
     }
+    
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        finish()
+        // Apply transition animation
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
 }
